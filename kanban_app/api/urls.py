65d1c2960254861d9a555b655ad61ce171f67fd8
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-08-06 08:35:07 +0200

    fixed urls

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     path('boards/', views.BoardListCreateView.as_view()),
-    path('boards/<int:pk>', views.BoardDetailView.as_view()),
+    path('boards/<int:pk>/', views.BoardDetailView.as_view()),
     path('tasks/', views.TaskList.as_view()),
     path('tasks/assigned-to-me/', views.AssignedToMeTaskList.as_view()),
     path('tasks/reviewing/', views.ReviewingList.as_view()),
-    path('tasks/<int:pk>', views.TaskDetail.as_view()),
+    path('tasks/<int:pk>/', views.TaskDetail.as_view()),
     path('tasks/<int:task_id>/comments/', views.CommentList.as_view()),
-    path('tasks/<int:task_id>/comments/<int:pk>', views.CommentDestroy.as_view())
+    path('tasks/<int:task_id>/comments/<int:pk>/',
+         views.CommentDestroy.as_view())
 ]
